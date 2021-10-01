5fcc109ac032b2c111b239be9f6cc7395fabea01
Anonixx <eddybraniff@gmail.com>
2021-10-01 00:36:44 -0400

    react

--- doggo-gram/src/components/Navbar/NavbarElements.js
+++ doggo-gram/src/components/Navbar/NavbarElements.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import { Link as LinkR } from 'react-router-dom'
+// import { Link} from 'react-scroll' 
 
 export const Nav = styled.nav`
 background: #000;
@@ -10,4 +12,30 @@ font-size: 1rem;
 position: sticky;
 top: 0;
 z-index: 10;
+
+@media screen and (max-width: 960px) {
+    transition: 0.8s all ease;
+}
 `
+
+export const NavbarContainer = styled.div`
+display: flex;
+justify-content: space-between;
+height: 80px;
+z-index: 1;
+width: 100%;
+padding: 0 24px;
+max-width: 1100px;
+`
+export const NavLogo = styled(linkR)`
+ color: #fff;
+ justify-self: flex-start;
+ cursor: pointer;
+ font-size: 1.5rem;
+ display: flex;
+ align-items: center;
+ margin-left: 24px;
+ font-weigt: bold;
+ text-decoration: none;
+`
+
